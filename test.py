02ba8ed129f37dddb6caf3a6368c183c25287333
Michelle <mlmichelleli7@gmail.com>
2020-01-11 18:29:46 -0800

    getting it to work with api

---
+++ test.py
@@ -0,0 +1,23 @@
+# Imports the Google Cloud client library
+from google.cloud import language
+from google.cloud.language import enums
+from google.cloud.language import types
+
+# Instantiates a client
+client = language.LanguageServiceClient()
+
+# The text to analyze
+#strText = u'This was the worst day of my life!'
+path = 'journal1.txt'
+f = open(path)
+text = f.read()
+
+document = types.Document(
+    content=text,
+    type=enums.Document.Type.PLAIN_TEXT)
+
+# Detects the sentiment of the text
+sentiment = client.analyze_sentiment(document=document).document_sentiment
+
+print('Text: {}'.format(text))
+print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
